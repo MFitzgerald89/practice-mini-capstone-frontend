b95a5864f9509b75f9fdf862b2683605393df864
MFitzgerald89 <122069535+MFitzgerald89@users.noreply.github.com>
2023-04-17 20:39:21 -0400

    filled header, productsnew, productsindex,footer, and content functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <header>
+        <a href="#">Home</a> | <a href="products-index">All Products</a> | <a href="products-new">New Product</a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+function ProductsNew() {
+  return (
+    <div id="products-new">
+      <h1>New Product</h1>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name"></input>
+        </label>
+      </form>
+      <form>
+        <label>
+          Description:
+          <input type="text" name="description"></input>
+        </label>
+      </form>
+      <form>
+        <label>
+          Price:
+          <input type="text" name="price"></input>
+        </label>
+      </form>
+      <form>
+        <label>
+          Image:
+          <input type="text" name="image_url"></input>
+        </label>
+      </form>
+    </div>
+  );
+}
+
+function ProductsIndex(props) {
+  console.log(props);
+  return (
+    <div id="products-index">
+      <h1>All Products</h1>
+      {props.products.map((product) => (
+        <div key={product.id} className="products">
+          <h2>{product.name}</h2>
+          <img src={product.image_url}></img>
+          <p>{product.description}</p>
+          <h3>{product.price}</h3>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright 2023</p>
+    </footer>
+  );
+}
+
+function Content() {
+  let products = [
+    {
+      name: "Catback Exhaust",
+      image_url: "https://www.parkmuffler.com/wp-content/uploads/2018/08/catback-exhaust.jpg",
+      description: "Part that flows exhaust gases out the back of a vehicle.",
+      price: 1200,
+    },
+    {
+      name: "Muffler",
+      image_url: "src/muffler.jpg",
+      description: "Part of exhaust system that muffles engine noise coming through exhaust pipes",
+      price: 450,
+    },
+  ];
+  return (
+    <div>
+      <ProductsNew />
+      <ProductsIndex products={products} />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
